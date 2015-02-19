0764d3aaa52d14d4fabab6a557dbbc5a3bc2b5ca
Billy Andika <bandika@ucsd.edu>
2015-02-18 18:11:49 -0800

    Fix bug with init DB

--- initPostsDB.js
+++ initPostsDB.js
@@ -23,22 +23,19 @@ mongoose.connect(database_uri);
 
 
 // Do the initialization here
-  console.log("first");
 
 // Step 1: load the JSON data
 var posts_json = require('./posts.json');
-console.log(posts_json);
+
 // Step 2: Remove all existing documents
 models.Post
   .find()
   .remove()
   .exec(onceClear); // callback to continue at
-  console.log("second");
 
 // Step 3: load the data from the JSON file
 function onceClear(err) {
   if(err) console.log(err);
-  console.log("here1");
   // loop over the posts, construct and save an object from each one
   // Note that we don't care what order these saves are happening in...
   var to_save_count = posts_json.length;
@@ -49,9 +46,8 @@ function onceClear(err) {
 
     proj.save(function(err, proj) {
       if(err) console.log(err);
-      // console.log("to_save_count" + to_save_count);
       to_save_count--;
-      // console.log(to_save_count + ' left to save');
+      console.log(to_save_count + ' left to save');
       if(to_save_count <= 0) {
         console.log('DONE');
         // The script won't terminate until the 
--- routes/index.js
+++ routes/index.js
@@ -13,17 +13,12 @@ var models = require('../models');
 
 
 exports.view = function(req, res){
-	console.log("00000");
 	models.Post
 		.find()
 		.exec(renderPosts);
-	console.log("1111");
-	//console.log(res);
 
 	function renderPosts(err, posts) {
 		console.log(err);
-		console.log(posts);
-		console.log("2222");
 		res.render('index', { 'posts': posts });
 	}
 
