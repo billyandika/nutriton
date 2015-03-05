4d56902478340a8a7e93e3d857f2858c0bce7d8b
Billy Andika <bandika@ucsd.edu>
2015-03-04 23:56:13 -0800

    Add alternative profile page

--- routes/profile.js
+++ routes/profile.js
@@ -1,3 +1,9 @@
 exports.view = function(req, res){
-	res.render('profile');
+  var random_num = Math.random();
+
+  if(random_num > 0.5){
+    res.render("profile");
+  }else{
+    res.render("profilealternative");
+  }
 };
