3a77ccfdc6812ae0c3e1ffaf983c7137c1506d1f
Billy Andika <bandika@ucsd.edu>
2015-03-07 17:20:39 -0800

    Add character count for new posts

--- public/js/nutriton.js
+++ public/js/nutriton.js
@@ -1,9 +1,5 @@
 'use strict';
 
-(function(){
-        var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
-})("woopra");
-
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
     initializePage();
@@ -13,6 +9,13 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+
+    $("#new-post-form .form-group .contentBox").keypress(function(){
+      if(this.value.length > 2000){
+          return false;
+      }
+      $("#remainingC").html("Remaining characters : " + (2000 - this.value.length));
+    });
 
     $(".version_a").click(function(){
       woopra.track("a_version_like_click");
