d5d29346fae99eff505db671592877d854317cc3
Billy Andika <bandika@ucsd.edu>
2015-03-07 21:32:55 -0800

    Edit profile and remove version B of profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,4 @@
 exports.view = function(req, res){
-  var random_num = Math.random();
-
-  if(random_num > 0.5){
-    res.render("profile");
-  }else{
-    res.render("profilealternative");
-  }
+	res.render('profile');
 };
+
